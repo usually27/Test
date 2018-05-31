24812cf806bc05fd133950a01e2074bcc4bbb3b6
usually27 <33239916+usually27@users.noreply.github.com>
2018-05-31 17:04:15 +0900

    Branch Test

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,8 +5,10 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		System.out.println("GitHub Test 클래스입니다.");
 		System.out.println("commit테스트");
-		System.out.println("------");
-		System.out.println("박용준테스트123");
+		System.out.println("박용준테스트12356");
+		System.out.println("테스트 ~~ 변경된거 라인만 되냐?");
+		System.out.println("테스트 . . . . . . ");
+		System.out.println("Branch에서 테서트");
 	}
 
 }
