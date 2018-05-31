3da63ff899752654a92d316ec31c1c3122717e21
usually27 <33239916+usually27@users.noreply.github.com>
2018-05-31 11:23:09 +0900

    Commit
    Commit test

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GitHub Test 클래스입니다.");
+		System.out.println("commit테스트");
 	}
 
 }
