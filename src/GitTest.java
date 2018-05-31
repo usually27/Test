54ab779d3f3367117ab69c4c8411fe53ca6825d4
usually27 <33239916+usually27@users.noreply.github.com>
2018-05-31 16:46:19 +0900

    eclipse에서 테스트

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,7 +5,8 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		System.out.println("GitHub Test 클래스입니다.");
 		System.out.println("commit테스트");
-		System.out.println("박용준테스트");
+		System.out.println("------");
+		System.out.println("박용준테스트123");
 	}
 
 }
